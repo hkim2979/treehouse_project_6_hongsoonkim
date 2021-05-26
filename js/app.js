ffd8160c0dbdfdf879a199954f7e3ce125c347e7
Hongsoon Kim <78128929+hkim2979@users.noreply.github.com>
2021-05-26 00:25:44 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const qwerty = document.getElementById("qwerty");
 const phrase = document.getElementById("phrase");
 const startButton = document.querySelector(".btn__reset");
 const overlay = document.getElementById("overlay");
+
 const phraseUl = phrase.firstElementChild;
 
 // missed variable
