984b78cd75ba2b700801a0e1b45a3f83eaaa348d
Hongsoon Kim <78128929+hkim2979@users.noreply.github.com>
2021-05-25 23:55:31 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 // variables
 
-const qwerty = document.getElementById('qwerty');
-const phrase = document.getElementById('phrase');
-const startButton = document.querySelector('btn__reset');
-const hideOverlay = document.getElementById('hideOverlay');
-const phraseUl = phrase.firstElementChild
+const qwerty = document.getElementById("qwerty");
+const phrase = document.getElementById("phrase");
+const startButton = document.querySelector(".btn__reset");
+const overlay = document.getElementById("overlay");
+const phraseUl = phrase.firstElementChild;
 
 // missed variable
 
@@ -12,56 +12,109 @@ let missed = 0;
 
 // event listener for btn_reset element
 
-startButton.addEventListener('click', () => {
-  hideOverlay.style.display = 'none';
+startButton.addEventListener("click", () => {
+  overlay.style.display = "none"
 });
 
 //phrase selection
 
 const phrases = [
-  'Adidas',
-  'Under Armour',
-  'Nike',
-  'New Balance',
-  'Puma'
+  "Nike",
+  "Adidas",
+  "Under Armour",
+  "Puma",
+  "New Balance"
 ];
 
 // getRandomPhraseAsArray function
 
 function getRandomPhraseAsArray(arr) {
-  const randomNumber = Math.floor(Math.random() * arr.length);
-  return arr[randomNumber].split('');
-}
+  const randomNumber = Math.floor(Math.random() * arr.length)
+  return arr[randomNumber].split("")
+};
 
-const phraseArray = getRandomPhraseAsArray(phrases);
+const phraseArray = getRandomPhraseAsArray(phrases)
 
 // addPhraseToDisplay function that loops through an array of characters
 
 function addPhraseToDisplay(arr) {
   for (let i = 0; i < arr.length; i++) {
-    const li = document.createElement('li');
-    li.textContent = arr[i];
-    phraseUl.appendChild(li);
-    if (arr[i] !== ' ') {
-      li.className = 'letter';
+    const li = document.createElement("li")
+    li.textContent = arr[i]
+    phraseUl.appendChild(li)
+    if (arr[i] !== " ") {
+      li.className = "letter"
     } else {
-      li.className = 'space';
+      li.className = "space"
     }
   }
-}
+};
 
 addPhraseToDisplay(phraseArray);
 
 // checkLetter function
 
-const letters = document.querySelectorAll('.letter');
-const checkLetter = (btn) => {
-  let letterMatched = null;
-  for (i = 0; i < letters.length; i++) {
-    if (btn === letters[i].textContent.toLowerCase()) {
-      letters[i].classList.add("show");
-      letterMatched = true;
+function checkLetter(btn) {
+  let letters = document.getElementsByClassName("letter")
+  let letterMatched = null
+  for (let i = 0; i < letters.length; i++) {
+    if (btn.textContent.toLowerCase() === letters[i].textContent.toLowerCase()) {
+      letters[i].classList.add("show")
+      letterMatched = letters[i].textContent.toLowerCase()
     }
   }
   return letterMatched;
-}
+};
+
+// event listener to the keyboard
+
+qwerty.addEventListener("click", (e) => {
+  if (e.target.tagName === "BUTTON") {
+    const btnPressed = e.target
+    btnPressed.classList.add("chosen")
+    btnPressed.setAttribute("disabled", true)
+    const letterMatched = checkLetter(e.target)
+    if (letterMatched === null) {
+      missed = missed + 1
+      document.querySelectorAll(".tries img")[missed - 1].src = "images/lostHeart.png"
+    }
+    checkWin()
+  }
+});
+
+// checkWin function for winning / losing display overlay
+
+function checkWin() {
+  let show = document.querySelectorAll(".show")
+  let letter = document.querySelectorAll(".letter")
+  if (missed > 4) {
+    overlay.className = "lose"
+    overlay.style.display = "flex"
+    document.querySelector("#overlay h2").textContent = "loser"
+    reset()
+  } else if (letter.length === show.length) {
+    overlay.className = "win"
+    overlay.style.display = "flex"
+    document.querySelector("#overlay h2").textContent = "winner"
+    reset()
+  }
+};
+
+//game reset function
+
+function reset() {
+  startButton.textContent = "do it again"
+  missed = 0
+  phraseUl.textContent = ""
+  const chosenLetters = document.querySelectorAll(".chosen")
+  for (let i = 0; i < chosenLetters.length; i++) {
+    chosenLetters[i].classList.remove("chosen")
+    chosenLetters[i].disabled = false
+  }
+  const phraseArray = getRandomPhraseAsArray(phrases)
+  addPhraseToDisplay(phraseArray)
+  let hearts = document.querySelectorAll(".tries img")
+  for (let i = 0; i < hearts.length; i++) {
+    hearts[i].src = "images/liveHeart.png"
+  }
+};
